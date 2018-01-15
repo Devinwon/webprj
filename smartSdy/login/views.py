ca76a007f776545d0cacbd1c516af04e84557183
Devin <917570905@qq.com>
2018-01-15 21:31:44 +0800

    reg need fix

--- smartSdy/login/views.py
+++ smartSdy/login/views.py
@@ -1,9 +1,11 @@
 # Create your views here.
 from django.shortcuts import render
+from django.contrib.auth.models import User
 from datetime import datetime
 from django.http import JsonResponse,HttpResponse,FileResponse
 from django.template import Template,Context
 from login.forms import Regfm
+
 
 def index(request):
 	return render(request,"index.html",{})
@@ -15,17 +17,24 @@ def reg(request):
 		context['regfm']=regfm
 		return render(request,'register.html',context)
 	else:
+		return HttpResponse("数据库维护中，请稍候...紧急可与管理员联系")
+
+		
+'''
 		username=request.POST.get('username')
 		password_set=request.POST.get('password_set')
 		password_confirm=request.POST.get('password_confirm')
 		email=request.POST.get('email')
 		if password_set==password_confirm:
-			Account.objects.create(
+			User.objects.create(
 			# Account.objects.create_user(
 				username=username,
 				password=password_set,
 				email=email,
 				)
-			return HttpResponse("Register Success")
+			
+			
+			
 		else:
 			return HttpResponse("Your password is not match")
+'''
