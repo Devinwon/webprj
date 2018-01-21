3943468060e55bb493dee5d08ce0435dedbbc311
Devin <917570905@qq.com>
2018-01-21 19:07:01 +0800

    static on server

--- smartSdy/smartSdy/settings.py
+++ smartSdy/smartSdy/settings.py
@@ -75,14 +75,14 @@ WSGI_APPLICATION = 'smartSdy.wsgi.application'
 
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'smartsdy',
-        'HOST': '127.0.0.1',
-        'PORT': 3306,
-        'USER': 'root',
-        'PASSWORD': 'root',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'smartsdy',
+        # 'HOST': '127.0.0.1',
+        # 'PORT': 3306,
+        # 'USER': 'root',
+        # 'PASSWORD': 'root',
     }
 }
 
@@ -124,6 +124,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#项目根目录
+STATIC_ROOT =  os.path.join(BASE_DIR,'static')
+
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "login\static"),
 ]
