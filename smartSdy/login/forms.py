49eb6136ab169117bd0d6f7879c7ee72045aa99a
Devin <917570905@qq.com>
2018-01-23 22:19:44 +0800

    reg

--- smartSdy/login/forms.py
+++ smartSdy/login/forms.py
@@ -2,27 +2,27 @@ from django import forms
 
 class Regfm(forms.Form):
     username = forms.CharField(
-      widget=forms.TextInput(attrs={"placeholder": "用户名", }),
-      max_length=10,
+      widget=forms.TextInput(attrs={"placeholder": "用户名5-20位", }),
+      max_length=20,
       min_length=5,
       error_messages={"required": "用户名不能为空",},
       label="注册用户"
       )
     password_set = forms.CharField(
-      widget=forms.PasswordInput(attrs={"placeholder": "设置密码",}),
+      widget=forms.PasswordInput(attrs={"placeholder": "设置密码6-20位",}),
       max_length=20,
       min_length=6,
       error_messages={"required": "密码不能为空",},
       label="输入密码")
     password_confirm = forms.CharField(
-      widget=forms.PasswordInput(attrs={"placeholder": "确认密码", "required": "required",}),
+      widget=forms.PasswordInput(attrs={"placeholder": "确认密码6-20位", "required": "required",}),
       max_length=20,
       min_length=6,
       error_messages={"required": "密码不能为空",},
       label="确认密码")
     email = forms.EmailField(
       widget=forms.EmailInput(attrs={"placeholder": "邮箱(选填)",}),
-      max_length=50,
+      max_length=20,
       min_length=7,
       error_messages={"required": "邮箱不能为空",},
       label="输入邮箱",
