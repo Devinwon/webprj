4c6ec000d9a4056fd31026da8e3cd88d4ae841dd
Devin <917570905@qq.com>
2018-01-23 20:23:25 +0800

    err info

--- smartSdy/login/views.py
+++ smartSdy/login/views.py
@@ -12,30 +12,23 @@ def index(request):
 
 def reg(request):
 	context={}
+	regfm=Regfm()       
+	context['regfm']=regfm
 	if request.method=='GET':
-		regfm=Regfm()       #创建一个注册类对象，get方法就直接传给html
-		context['regfm']=regfm
 		return render(request,'register.html',context)
 	else:
 		try:
 			regfm=Regfm(request.POST)		#获取用户提交数据
-			# print("regfm",regfm)
-			#合法性判断,只是对定义规则做了检查，密码匹配，用户重复均没有检查
-			# namefilter = User.objects.filter(username=username,password=password)检查用户是否存在
 			if regfm.is_valid():
 				password_set=request.POST.get('password_set')
 				password_confirm=request.POST.get('password_confirm')
-				# print("is_valid,here")
 				
 				if password_set==password_confirm:
 					username=request.POST.get('username')
 					userResult = User.objects.filter(username__exact=username)
-					# print(userResult,'user')
 					if userResult:
 						err="用户名"+username+"已经存在"
 						context["err"]=err
-						print("err,",err)
-						# return HttpResponse("user exist，contact with master ")
 					else:
 						email=request.POST.get('email')
 						User.objects.create_user(
@@ -44,16 +37,11 @@ def reg(request):
 							email=email,
 							)
 						return HttpResponse("register success，contact with master ")
-					
 				else:
 					err="两次密码不一致"
-					# print("run here...")
 					context["err"]=err
-			regfm=Regfm()
-			context["regfm"]=regfm
+
 			return render(request,'register.html',context)
-				
 			# return HttpResponse("invalid data，contact with master ")
-			# return HttpResponse("register failed，contact with master ")
 		except:
-			return HttpResponse("register failed，contact with master ")
+			return render(request,'register.html',context)
